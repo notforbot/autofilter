5d571709c77c9c5153471baedeb710cfd7dcb2fd
notforbot <126742876+notforbot@users.noreply.github.com>
2023-03-02 17:50:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = 'mongodb+srv://sidgithubforbot:QsPxAkXxqpRYhYap@cluster0.9w4o4od.mongodb.net/?retryWrites=true&w=majority'
+DATABASE_URI = 'mongodb+srv://Gojohu:NVVSit5RAP4D139y@cluster0.ydmhxkm.mongodb.net/?retryWrites=true&w=majority'
 DATABASE_NAME = environ.get('DATABASE_NAME', "LazyDevelopers")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
